009fc7c3218aade562d8144196a7c5964d11cc7b
Froi Nunez <froinunez@Frois-MacBook-Pro.local>
2024-06-10 15:36:26 -0400

    Adding cursor effect and social media link

--- script.js
+++ script.js
@@ -29,3 +29,57 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+//Cursor effect
+document.documentElement.style.cursor = "none";
+const circleElement = document.querySelector(".circle");
+const mouse = { x: 0, y: 0 };
+const previousMouse = { x: 0, y: 0 }; // Store the previous mouse position
+const circle = { x: 0, y: 0 };
+let currentScale = 0;
+let currentAngle = 0;
+
+window.addEventListener("mousemove", (e) => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+const speed = 0.17;
+
+const tick = () => {
+  circle.x += (mouse.x - circle.x) * speed;
+  circle.y += (mouse.y - circle.y) * speed;
+  const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
+
+  // let previousMouse = { x : null, y : null};
+
+  //Squeeze
+  const deltaMouseX = mouse.x - previousMouse.x;
+  const deltaMouseY = mouse.y - previousMouse.y;
+  previousMouse.x = mouse.x;
+  previousMouse.y = mouse.y;
+
+  const mouseVelocity = Math.min(
+    Math.sqrt(deltaMouseX ** 2 + deltaMouseY ** 2) * 4,
+    150
+  );
+  const scaleValue = (mouseVelocity / 150) * 0.5;
+  currentScale += (scaleValue - currentScale) * speed;
+
+  const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
+
+  //Rotate
+  const angle = (Math.atan2(deltaMouseY, deltaMouseX) * 180) / Math.PI;
+  const rotateTransform = `rotate(${angle}deg)`;
+
+  if (mouseVelocity > 20) {
+    currentAngle = angle;
+  }
+
+  //Apply transforms
+  circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
